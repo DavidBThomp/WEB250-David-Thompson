ea3bb70dd40ad0f2ce3df3c36314b3b674e58024
David Thompson <davidbthomp@gmail.com>
2021-02-28 14:45:29 -0600

    Lesson 8 Annotated

--- Assignment_8/routes/lesson8.js
+++ Assignment_8/routes/lesson8.js
@@ -51,10 +51,16 @@ router.get("/", async function (request, response) {
 
 async function getData() {
     try {
-        let data = await getRawData(URL, QUERY);
+        let data = await getRawData(URL, QUERY); 
+        // Waits to get data from URL
+
         let records = getRecords(data);
+        // Sorts the info in key,value format
+
         records.sort(function(a, b) {return b.celsius - a.celsius});
         let result = formatTable(records);
+
+        //Sorts table in order of celsius and puts table in the variable result
         return result;
     }
     catch(error) {
@@ -65,35 +71,53 @@ async function getData() {
 async function getRawData(url, query) {
     url += "?query=" + encodeURI(query);
     url += "&format=json";
+    //formats the url to download as querty in json format
 
     return await fetch(url)
         .then(response => response.json());
+        //Returns the download information from this query
 }
 
 function getRecords(data) {
     let records = [];
+    //creates an array fot the records from the data of download
     for (let i = 0; i < data.results.bindings.length; i++) {
         record = getRecord(data.results.bindings[i]);
+        // goes through the result of data and 
+        // .results shows data from bindings
+        // .bindings displays data in the bindings (country and Maxiumum Temp)
         records.push(record);
+        // Put values from record below into array
     }
     return records;
 }
 
 function getRecord(object) {
+    // object is data.results.binding[i]
+    
     let country = object.Country.value;
+    // Extracts the value of country binding
+
     let celsius = object.MaximumTemperature.value;
+    // Extracts the value of Maximum Temperature Binding
+
     let index = celsius.indexOf(" °C");
     if (index < 0) {
         throw "Invalid data format";
     }
+    // If a celsius value doesn't have then invalid
 
     celsius = Number(celsius.substring(0, index));
-    let fahrenheit = celsius * 9 / 5 + 32;
+    // Celsius in number ignoring all else
 
-    let record = {};
+    let fahrenheit = celsius * 9 / 5 + 32;
+    // Fahrenheit Temp
+
+    let record = {}; //Associative array
     record.country = country;
     record.celsius = celsius;
     record.fahrenheit = fahrenheit;
+    // All 3 values in record
     return record;
 }
 
@@ -101,6 +125,7 @@ function formatTable(records) {
     let result = "<table><tr><th>Country</th>"
     result += "<th>Celsius</th>";
     result += "<th>Fahrenheit</th></tr>";
+    // Top Row of Table
 
     for (index = 0; index < records.length; index++) {
         let record = records[index];
@@ -108,8 +133,11 @@ function formatTable(records) {
         result += "<td>" + record.celsius.toFixed(1) + "° C</td>";
         result += "<td>" + record.fahrenheit.toFixed(1) + "° F</td></tr>";        
     }
-
+    // Input values for table
+    
     result += "</table>";
+    // End table
+
     return result;
 }
 
