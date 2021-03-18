129d2612e92e690cb58d047b84fe9a92da630ea9
David Thompson <davidbthomp@gmail.com>
2021-03-17 19:41:24 -0500

    HOST address update

--- Assignment_10/routes/lesson10.js
+++ Assignment_10/routes/lesson10.js
@@ -24,7 +24,7 @@ const router = express.Router();
 
 // const HOST = "mongodb://172.17.0.2";
 // mongodb://localhost:27017 for Local server
-const HOST = "mongodb://localhost:27017";
+const HOST = "mongodb://172.17.0.2";
 const DATABASE = "pizzaOrder";
 const COLLECTION = "orders";
 
